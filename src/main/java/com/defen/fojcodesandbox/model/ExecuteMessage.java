b5e699e3865ed08773e492adcf22eebd74d9c797
chendefeng <1257340960@qq.com>
2025-09-06 17:27:26 +0800

    feat：新增代码沙箱执行功能

---
+++ src/main/java/com/defen/fojcodesandbox/model/ExecuteMessage.java
@@ -0,0 +1,30 @@
+package com.defen.fojcodesandbox.model;
+
+import lombok.Data;
+
+/**
+ * 进程执行信息
+ */
+@Data
+public class ExecuteMessage {
+
+    /**
+     * 退出码
+     */
+    private Integer exitValue;
+
+    /**
+     * 正常信息
+     */
+    private String message;
+
+    /**
+     * 错误信息
+     */
+    private String errorMessage;
+
+    /**
+     * 执行时间
+     */
+    private Long time;
+}
